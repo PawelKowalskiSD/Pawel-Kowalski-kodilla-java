3a1a286bcf632c33b9e7d46c98fccca2d4e009e6
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2022-12-07 21:28:59 +0100

    6.3 Select class test counts from collection

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,35 +1,7 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.calculator.Calculator;
-import com.kodilla.testing.user.SimpleUser;
-
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator(20, 10);
-
-        int addA = calculator.add(calculator.getA(), calculator.getB());
-        int subB = calculator.subtract(calculator.getA(), calculator.getB());
-
-        if (addA == 30) {
-            System.out.println("test Ok");
-        } else {
-            System.out.println("Error!");
-        }
-
-        if(subB == 10) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,60 @@
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.*;
+import java.util.Arrays;
+import java.util.List;
+
+@DisplayName("Collection Test Suite")
+public class CollectionTestSuite {
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Case: begin");
+        System.out.println("=================");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("=================");
+        System.out.println("Test Case: end"  + "\n");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Test Suite: begin");
+        System.out.println("=================" + "\n");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("=================");
+        System.out.println("Test Suite: end");
+    }
+
+    @DisplayName(" test empty list")
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
+        List<Integer> newEmptyList = Arrays.asList();
+        List<Integer> emptyList = oddNumbersExterminator.exterminate(newEmptyList);
+        System.out.println("Empty list is: " + emptyList.size());
+        //Then
+        Assertions.assertTrue(emptyList.isEmpty());
+    }
+
+    @DisplayName("test checks the method whether it splits the numbers")
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
+        List<Integer> newList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        List<Integer> evenNumbers = oddNumbersExterminator.exterminate(newList);
+        System.out.println("All number on the list: " + newList);
+        System.out.println("After filtering out the odd numbers: " + evenNumbers);
+        //Then
+        Assertions.assertNotEquals(newList,evenNumbers);
+        Assertions.assertNotSame(newList,evenNumbers);
+    }
+}
