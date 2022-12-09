5c6cc628a1185fd99c5025a69fcbbe4d419877e9
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2022-12-09 07:43:05 +0100

    6.3 Select class test counts from collection

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,6 +1,8 @@
 package com.kodilla.testing.collection;
 
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -35,11 +37,11 @@ public class CollectionTestSuite {
     void testOddNumbersExterminatorEmptyList() {
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        List<Integer> newEmptyList = new ArrayList<>();
         //When
-        List<Integer> newEmptyList = Arrays.asList();
         List<Integer> emptyList = oddNumbersExterminator.exterminate(newEmptyList);
-        System.out.println("Empty list is: " + emptyList.size());
         //Then
+        System.out.println("Empty list is: " + emptyList.size());
         Assertions.assertTrue(emptyList.isEmpty());
     }
 
@@ -48,12 +50,12 @@ public class CollectionTestSuite {
     void testOddNumbersExterminatorNormalList() {
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
-        //When
         List<Integer> newList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
-        List<Integer> evenNumbers = oddNumbersExterminator.exterminate(newList);
         System.out.println("All number on the list: " + newList);
-        System.out.println("After filtering out the odd numbers: " + evenNumbers);
+        //When
+        List<Integer> evenNumbers = oddNumbersExterminator.exterminate(newList);
         //Then
+        System.out.println("After filtering out the odd numbers: " + evenNumbers);
         Assertions.assertNotEquals(newList,evenNumbers);
         Assertions.assertNotSame(newList,evenNumbers);
         Assertions.assertEquals(evenNumbers, Arrays.asList(2, 4, 6, 8));
