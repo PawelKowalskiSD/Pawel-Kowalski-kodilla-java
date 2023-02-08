895941ca710b69ae15a6deb4d764e14e21da31bc
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2023-02-08 13:22:55 +0100

    Task 14.2 display calculator

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+
+    @Autowired
+    private Calculator calculator;
+
+    @Test
+    void testCalculations() {
+        //Given
+
+        //When
+        calculator.add(1,1);
+        calculator.sub(2,1);
+        calculator.mul(2,2);
+        calculator.div(4,2);
+        //Then
+        //do nothing
+    }
+}
