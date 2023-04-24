ce97708ef4d02da7abb4e29922c9148ae5ed389b
Pawel Kowalski <kowalski.pawel.sd@gmail.com>
2023-04-24 22:37:36 +0200

    Task 24.5

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -0,0 +1,27 @@
+package com.kodilla.patterns2.adapter.bookclassifier;
+
+import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class MedianAdapterTestSuite {
+
+    @Test
+    public void testPublicationYearMedian() {
+        //Given
+        Set<Book> books = new HashSet<>();
+        for (int i = 0; i < 120; i++) {
+            books.add(new Book("Author" + i, "title" + i, 1910 + i, "signature" + 1));
+        }
+        MedianAdapter medianAdapter = new MedianAdapter();
+
+        //When
+        int publicationYearMedian = medianAdapter.publicationYearMedian(books);
+
+        //Then
+        System.out.println(publicationYearMedian);
+    }
+
+}
